1919c24bc91b73a0d13ca56d0a1e206d36538b57
tttrono <theiss.thella.trono@student.ateneo.edu>
2025-04-25 22:46:03 +0800

    Modified settings to include user_management.

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'merchstore',
-    'commissions'
+    'commissions',
+    'user_management'
 ]
 
 MIDDLEWARE = [
--- hobbysite/user_management/models.py
+++ hobbysite/user_management/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, one_delete=models.CASCADE)
+    display_name = models.CharField(max_length=63)
+    email = models.EmailField(max_length=254)
+    
+    def _str_(self):
+        return self.name
