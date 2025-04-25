32d440700221092c37401e3a2a7a50c429b4f361
tttrono <theiss.thella.trono@student.ateneo.edu>
2025-04-26 00:11:14 +0800

    Modified for handling static files.

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -125,6 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR/'hobbysite/static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -133,3 +136,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/accounts/login"
+
