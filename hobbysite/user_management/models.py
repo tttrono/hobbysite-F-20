7903045ce13e9a9757133125fd096c5e1ad093c6
tttrono <theiss.thella.trono@student.ateneo.edu>
2025-04-25 22:55:57 +0800

    Added model for User Profile.

--- hobbysite/user_management/models.py
+++ hobbysite/user_management/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
+    """ A model for user profile. """
     user = models.OneToOneField(User, one_delete=models.CASCADE)
     display_name = models.CharField(max_length=63)
     email = models.EmailField(max_length=254)
