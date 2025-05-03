97e775e0ea6ce3b779ecdde430fa2c40a9b31454
tttrono <theiss.thella.trono@student.ateneo.edu>
2025-05-03 23:33:17 +0800

    Updated templates to load bootstrap css.

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_bootstrap5',
     'commissions'
 ]
 
@@ -122,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'hobbysite/static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
