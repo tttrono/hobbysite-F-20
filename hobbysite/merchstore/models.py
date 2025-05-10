66db4f90141b8ca9a608f8fa1b307d780c2d988b
tttrono <theiss.thella.trono@student.ateneo.edu>
2025-05-10 11:37:52 +0800

    Modified Product model to add status and owner field.

--- hobbysite/merchstore/models.py
+++ hobbysite/merchstore/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.urls import reverse
 from enum import unique
 
+from user_management.models import Profile
+
 class ProductType(models.Model):
     """A model for product types or categories."""
     name = models.CharField(max_length=255, unique=True)
@@ -18,8 +20,17 @@ class Product(models.Model):
     """A model for products in the merch store."""
     name = models.CharField(max_length=255, unique=True)
     product_type = models.ForeignKey(ProductType, null=True, on_delete=models.SET_NULL, related_name='product')
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='product')
     description = models.TextField()
     price = models.DecimalField(max_digits=9, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    
+    class Status(models.TextChoices):
+        AVAILABLE = 'available', 'Available'
+        ON_SALE = 'on_sale', 'On Sale'
+        OUT_OF_STOCK = 'out_of_stock', 'Out of Stock'
+    
+    status = models.CharField(max_length=20, choices=Status.choices, default=Status.AVAILABLE)
     
     class Meta:
         verbose_name_plural = 'Products'
@@ -30,4 +41,6 @@ class Product(models.Model):
     
     def get_absolute_url(self):
         return reverse('merchstore:item', args=[self.pk])
+    
+
         
