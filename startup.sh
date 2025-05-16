669d569f4e38b305677318d1527dc02904b656b4
tttrono <theiss.thella.trono@student.ateneo.edu>
2025-05-17 00:52:24 +0800

    Fixed startup file.

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python manage.py migrate
 python manage.py collectstatic --noinput
-gunicorn --bind=https://hobbysite-f-20-g0gqa7acascyhed2.southeastasia-01.azurewebsites.net/ --timeout 600 hobbysite.wsgi --access-logfile '-' --error-logfile '-'
+gunicorn --bind=0.0.0.0 --timeout 600 hobbysite.wsgi:application
