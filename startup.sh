35a98c9cfcf8635f3a727008a775584de85f66ae
tttrono <theiss.thella.trono@student.ateneo.edu>
2025-05-17 17:45:56 +0800

    Added CPU cores for app service.

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python manage.py migrate
 python manage.py collectstatic --noinput
-gunicorn --bind=0.0.0.0 --timeout 600 hobbysite.wsgi:application
+gunicorn --bind=0.0.0.0 --timeout 600 --workers=4 hobbysite.wsgi:application
