0ddab0905232623f015ed06980fbb9968793a015
tttrono <theiss.thella.trono@student.ateneo.edu>
2025-05-23 21:58:33 +0800

    Moved app-level urls to main settings.

--- hobbysite/user_management/urls.py
+++ hobbysite/user_management/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
-    path('profile/<int:pk>/', ProfileUpdateView.as_view(), name='update'),
+    path('<int:pk>/', ProfileUpdateView.as_view(), name='update'),
 ]
 
 app_name = 'user_management'
